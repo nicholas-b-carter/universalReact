22e2de126d48d1d4550bbb08bda740275fa92173
Nick Carter <nicholas.b.carter@gmail.com>
2017-11-16 08:09:32 -0500

    we think this may work for windows

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
-import ReactNative from 'react-native'
-import App from './App'
+// entry
+import React from "react";
+import { Provider } from "react-redux";
+import { AppRegistry, Platform } from "react-native";
+import { Router, AndroidBackButton } from "./common/routing";
+import registerServiceWorker from "./common/web/registerServiceWorker";
+import store from "./common/store";
+import App from "./containers/app";
+import Welcome from "./containers/welcome";
 
-ReactNative.render(<App/>, document.getElementById('root'));
+const main = props => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <AndroidBackButton>
+          <App statusBar={false}>
+            <Welcome />
+          </App>
+        </AndroidBackButton>
+      </Router>
+    </Provider>
+  );
+};
+
+export default main;
+
+if (Platform.OS === "web") {
+  AppRegistry.registerComponent("rnb", () => main);
+  AppRegistry.runApplication("rnb", {
+    rootTag: document.getElementById("root")
+  });
+  registerServiceWorker();
+}
